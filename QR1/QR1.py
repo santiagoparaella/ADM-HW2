394a5ae4f7bab9c6dbd857ee956bd4d967216e57
santiago-sapienza <32667384+santiagoparaella@users.noreply.github.com>
2019-10-18 17:56:02 +0200

    Add files via upload

--- QR1/QR1.py
+++ QR1/QR1.py
@@ -1,7 +1,3 @@
-'''
-This file was written by Santo Palaia and explained and shared with Greeshma and Pooja
-'''
-
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -90,6 +86,7 @@ for index, row in teamMatchWin.iterrows(): #inizilization of a dictionary with k
 
 #we  want iterate over whe weeks to add to the dict a pair of (week, score) in order to obtain the point x, y for a plot
 
+
 for i in range(1, 39):
     weekI=teamMatchWin.loc[teamMatchWin['gameweek']==i]
     weekIT=teamMatchTied.loc[teamMatchTied['gameweek']==i]
@@ -117,6 +114,84 @@ for i in range(1, 39):
         newScore=listOfTeam[len(listOfTeam)-1][1]+1
         d[row.loc['name_2']].append([i, newScore])
 
+#looking for longhest winning streak and longhest loosing streak
+win1=''
+win2=''
+los1=''
+los2=''
+
+#winner
+
+w=''
+nw=0
+for j in range(2):
+    for k, v in d.items():
+        #print(k)
+        n=0
+        prev=v[0]
+        for i in range(1, len(v)):
+            att=v[i]
+            if att[0]==prev[0]+1:
+                n+=1
+            else:
+                if n>nw and k!=win1:
+                    nw=n
+                    w=k
+                n=0
+            prev=att
+
+
+    nw=0
+    if j==0:
+        win1=w
+    else:
+        win2=w
+
+#print('win1 ', win1, 'win2', win2)
+
+#save data of two "winner" to show them separately
+w1=d[win1]
+vec1=[i for [i, j] in w1]
+wee1=[j for [i, j] in w1]
+w2=d[win2]
+vec2=[i for [i, j] in w2]
+wee2=[j for [i, j] in w2]
+
+
+#lost
+w=''
+nw=0
+for j in range(2):
+    for k, v in d.items():
+        #print(k)
+        n=0
+        prev=v[0]
+        for i in range(1, len(v)):
+            att=v[i]
+
+            n=att[0]-prev[0]
+            if n>nw and k!=los1:
+                nw=n
+                w=k
+            n=0
+            prev=att
+
+
+    nw=0
+    if j==0:
+        los1=w
+    else:
+        los2=w
+
+#print('l1 ', los1, 'l2', los2)
+
+#save data of two "lores" to show them separately
+l1=d[los1]
+vec1l=[i for [i, j] in l1]
+wee1l=[j for [i, j] in l1]
+l2=d[los2]
+vec2l=[i for [i, j] in l2]
+wee2l=[j for [i, j] in l2]
 
 
 #delete the firt element \[0, 0\] or replace with \[1, 0\] used to star the line from 0 at the firs week
@@ -133,21 +208,33 @@ for v in d.values():
 
 
 heigth=110
-plt.axis([0, 39, 0, heigth])
-plt.xticks(range(40), ['week '+str(i) for i in range(39)], rotation=30, fontsize='xx-small')
-plt.yticks(list(range(0, heigth, 5)))
+plt.axis([0, 39, 0, heigth]) #set the length of axis
+plt.xticks(range(40), ['week '+str(i) for i in range(39)], rotation=30, fontsize='xx-small') #set the x-ticks, them text and other things
+plt.yticks(list(range(0, heigth, 5))) #set the y-ticks
 
+#we have to plot before the two winners and loosers
+plt.plot(vec1, wee1, '*-',  label=win1)
+plt.plot(vec2, wee2, '*-',  label=win2)
+plt.plot(vec1l, wee1l, '*-', label=los1)
+plt.plot(vec2l, wee2l, '*-', label=los2)
+#cause we can remove them from dictionary and then print the remaining teams
+del d[win1]
+del d[win2]
+del d[los1]
+del d[los2]
 
-
+#print of the remaining team
 for k, v in d.items():
     #vector=[j for [i, j] in v]
     #week=[i for [i, j] in v]
-    #print(v)
+    #print(k, v)
     plt.plot([i for [i, j] in v], [j for [i, j] in v], label=k)
 
 
+#add legend
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
+#save picture: the path and file name, the resolution, and the box to fit the image around legend and plot
 plt.savefig('/home/tiago/Scrivania/Libri Magistrale/1st semester/ADM/HomeWork2/matches/results', format='png', dpi=500, bbox_inches='tight')
 
 
