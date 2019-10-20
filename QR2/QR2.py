9055c58cff5db7870495e0582dd79487b557b3b3
santiago-sapienza <32667384+santiagoparaella@users.noreply.github.com>
2019-10-20 17:14:17 +0200

    Add files via upload

--- QR2/QR2.py
+++ QR2/QR2.py
@@ -81,14 +81,17 @@ with open('/home/tiago/Scrivania/Libri Magistrale/1st semester/ADM/HomeWork2/mat
     print('Test 1\n', file=f)
     contTable=get_team_contingecy_table(Manchester_Utd)
     chi2_1, p_1, dof_1, exp_1 = chi2_contingency(contTable, correction=False)
+    print('Contingency Table\n', contTable, file=f)
     print('chi2: ', chi2_1, file=f)
     print('p: ', p_1, file=f)
     print('dof: ', dof_1, file=f)
     print('expeted: ','\n', exp_1, file=f)
     answer_ariss_Question(p_1)
+    print('', file=f)
     
     #Manchester_city
     print('Test 2\n', file=f)
+    print('Contingency Table\n', contTable, file=f)
     contTable=get_team_contingecy_table(Manchester_city)
     chi2_2, p_2, dof_2, exp_2 = chi2_contingency(contTable, correction=False)
     print('chi2: ', chi2_2, file=f)
@@ -96,9 +99,11 @@ with open('/home/tiago/Scrivania/Libri Magistrale/1st semester/ADM/HomeWork2/mat
     print('dof: ', dof_2, file=f)
     print('expeted: ','\n', exp_2, file=f)
     answer_ariss_Question(p_2)
+    print('', file=f)
     
     #Liverpool
     print('Test 3\n', file=f)
+    print('Contingency Table\n', contTable, file=f)
     contTable=get_team_contingecy_table(Liverpool)
     chi2_3, p_3, dof_3, exp_3 = chi2_contingency(contTable, correction=False)
     print('chi2: ', chi2_3, file=f)
@@ -106,9 +111,11 @@ with open('/home/tiago/Scrivania/Libri Magistrale/1st semester/ADM/HomeWork2/mat
     print('dof: ', dof_3, file=f)
     print('expeted: ','\n', exp_3, file=f)
     answer_ariss_Question(p_3)
+    print('', file=f)
     
     #Arsenal
     print('Test 4\n', file=f)
+    print('Contingency Table\n', contTable, file=f)
     contTable=get_team_contingecy_table(Manchester_Utd)
     chi2_4, p_4, dof_4, exp_4 = chi2_contingency(contTable, correction=False)
     print('chi2: ', chi2_4, file=f)
@@ -116,9 +123,11 @@ with open('/home/tiago/Scrivania/Libri Magistrale/1st semester/ADM/HomeWork2/mat
     print('dof: ', dof_4, file=f)
     print('expeted: ','\n', exp_4, file=f)
     answer_ariss_Question(p_4)
+    print('', file=f)
     
     #Chelsea
     print('Test 5\n', file=f)
+    print('Contingency Table\n', contTable, file=f)
     contTable=get_team_contingecy_table(Manchester_Utd)
     chi2_5, p_5, dof_5, exp_5 = chi2_contingency(contTable, correction=False)
     print('chi2: ', chi2_5, file=f)
@@ -126,3 +135,8 @@ with open('/home/tiago/Scrivania/Libri Magistrale/1st semester/ADM/HomeWork2/mat
     print('dof: ', dof_5, file=f)
     print('expeted: ','\n', exp_5, file=f)
     answer_ariss_Question(p_5)
+    print('', file=f)
+    
+    print('Concludions:\n', file=f)
+    print("we have too little data to carry out a meaningful analysis.", file=f)
+    print("We should also carry out further tests in addition to chi2 in order to better investigate the correlation between the two variables analysed", file=f)
